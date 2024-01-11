305352d2eef2d3887bd8221baad50c45adc8519a
rileyhaspowers <rileyhaspowers@example.com>
2024-01-11 17:06:05 -0600

    Finished part 1 of first graded assignment

--- candidate-testing.js
+++ candidate-testing.js
@@ -4,10 +4,11 @@ const input = require('readline-sync');
 
 // TODO 1.1a: Define candidateName // 
 let candidateName;
+candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
@@ -18,31 +19,40 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+  const input = require('readline-sync');
+  let candidateName = input.question("Please enter your name.");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
+    const input = require('readline-sync');
+    let candidateAnswer = input.question(question);
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+      if (candidateAnswer === "Sally Ride" || "sally ride" || "Sally ride"){
+          console.log("Correct! You got 1/1 questions right.")
+      } else {
+            console.log("You got one or more of the questions incorrect. Please review your answers below.")
+          }
+      }
 
 
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
 
-  return grade;
-}
+  //return grade;
+
 
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log();
+   console.log("Greetings, " + candidateName);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
